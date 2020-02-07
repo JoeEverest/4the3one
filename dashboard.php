3df5cefb94d20c9c69bacd9761d4ca4068eaeed7
JoeEverest <everestjoseph11@yahoo.com>
2020-02-07 17:13:12 +0300

    styling dashboard

--- dashboard.php
+++ dashboard.php
@@ -6,9 +6,26 @@ include("includes/config.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="assets/js/jquery-3.4.1.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/dashboard.css">
+    <link rel="stylesheet" href="assets/css/Contact-Form-Clean.css">
+    <link rel="stylesheet" href="assets/css/ebs-contact-form.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Dashboard</title>
 </head>
 <body>
+
+<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+</nav>
+<div class="container">
+    <br>
+    <h2>SELECT YOUR FIELD</h2>
     <?php
     $getCategory = "SELECT * FROM fields ORDER BY id ASC";
     $getCategory =  mysqli_query($connect, $getCategory);
@@ -20,5 +37,8 @@ include("includes/config.php");
     <?php
     }
     ?>
+</div>
+
+    
 </body>
 </html>
--- paths.php
+++ paths.php
@@ -2,8 +2,14 @@
 include("includes/config.php");
 if (isset($_GET['id'])) {
     $categoryid = $_GET['id'];
+    $getfield = "SELECT * FROM fields WHERE id = '$categoryid' ORDER BY id ASC";
+    $getfield =  mysqli_query($connect, $getfield);
+    while ($rq = mysqli_fetch_array($getfield)) {
+        $id = $rq['id'];
+        $name = $rq['name'];
+    }
 }else{
-    header("Location: dashbosrd.php");
+    header("Location: dashboard.php");
 }
 ?>
 <!DOCTYPE html>
@@ -11,7 +17,7 @@ if (isset($_GET['id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
+    <title>Paths in: <?php echo $name ?></title>
 </head>
 <body>
     <?php
@@ -21,7 +27,9 @@ if (isset($_GET['id'])) {
         $id = $row['id'];
         $name = $row['name'];
     ?>
-    <div class="item"><a href="content.php?id=<?php echo $id; ?>"><?php echo $name; ?></a></div>
+    <div class="item">
+        <a href="content.php?id=<?php echo $id; ?>"><?php echo $name; ?></a>
+    </div>
     <?php
     }
     ?>
